595ee807b01292b892e09c5623538e5cbe46f818
Jeonghoon2 <ukidd12@naver.com>
2023-06-24 21:24:09 +0900

    [TEST] 도메인 Repository

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -27,6 +29,37 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+
+    }
+
+    @Test
+    public void basicCRUD() throws Exception{
+        Member member1 = new Member("memberA");
+        Member member2 = new Member("memberB");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        /* 리스트 조회 검증 */
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        /* 카운트 검증 */
+        long afterCount = memberJpaRepository.count();
+        assertThat(afterCount).isEqualTo(2);
+
+        /* 맴버 삭제 검증 */
+        memberJpaRepository.remove(member1);
+        memberJpaRepository.remove(member2);
+
+        long beforeCount = memberJpaRepository.count();
+        assertThat(beforeCount).isEqualTo(0);
+
 
     }
 
