7f4dab513f212131a606de89c1f2794035f0d600
Jeonghoon2 <ukidd12@naver.com>
2023-06-24 21:07:25 +0900

    도메인 Repository CRUD

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -14,6 +17,29 @@ public class MemberJpaRepository {
     public Member save(Member member){
         em.persist(member);
         return member;
+    }
+
+    /* 특정 삭제 */
+    public void remove(Member member){
+        em.remove(member);
+    }
+
+    /* 모두 조회 */
+    public List<Member> findAll(){
+        /* JPQL */
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    /* 특정 조회*/
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id){
